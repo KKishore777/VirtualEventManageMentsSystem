6051606e30f7168c5891d20422bda306199cba64
Kandhukuru Kishore <kishorekandhukuru@gmail.com>
2026-04-15 15:12:33 +0530

    Add files via upload

---
+++ AppUI/Models/ParticipantDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AppUI.Models
+{
+    public class ParticipantDashboardViewModel
+    {
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventCategory { get; set; }
+
+        public string SessionTitle { get; set; }
+        public DateTime SessionStart { get; set; }
+
+        public string Description { get; set; }
+        public bool IsAttended { get; set; }
+    }
+}
